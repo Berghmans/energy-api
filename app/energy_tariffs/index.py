aac29d99e7ab8bf69cab300d7c1769807a4645a2
Olivier Berghmans <olivier@cloudar.be>
2023-05-10 11:27:42 +0200

    Add functionality for calculating end price

--- app/energy_tariffs/index.py
+++ app/energy_tariffs/index.py
@@ -12,25 +12,47 @@ def get_index_values() -> list[EngieIndexingSetting]:
     return EngieIndexingSetting.from_url(GAS_URL) + EngieIndexingSetting.from_url(ENERGY_URL)
 
 
-def handler(event, _context):
-    """The handler"""
-    last_month = date.today().replace(day=1) - timedelta(days=1)
-    req_index = event["INDEX"]
-    req_year = event.get("YEAR", last_month.year)
-    req_month = event.get("MONTH", last_month.month)
+def get_indexing_setting(index_name: str, year: int, month: int) -> EngieIndexingSetting:
+    """Get the indexing setting"""
     index_values = get_index_values()
-
     result = [
         index_value
         for index_value in index_values
-        if index_value.date.year == req_year and index_value.date.month == req_month and index_value.name == req_index
+        if index_value.date.year == year and index_value.date.month == month and index_value.name == index_name
     ]
     if len(result) == 1:
-        return {"statusCode": 200, "body": json.dumps(asdict(result[0]), default=str)}
+        return result[0]
+    raise ValueError("None or more than one found")
 
 
-if __name__ == "__main__":
-    print(handler({"INDEX": "ZTP DAM"}, {}))
-    print(handler({"INDEX": "Epex DAM"}, {}))
-    print(handler({"INDEX": "ZTP DAM", "YEAR": 2023, "MONTH": 3}, {}))
-    print(handler({"INDEX": "Epex DAM", "YEAR": 2023, "MONTH": 3}, {}))
+def get_end_price(index_name: str, year: int, month: int, slope: float, intercept: float, taxes: float) -> float:
+    """Calculate the end price"""
+    # Using linear regression Y = a + bX
+    # a: the intercept, which the base cost of the unit
+    # b: the slope of the line, which is the factor to be multiplied with the indexing setting value
+    end_price = intercept + slope * get_indexing_setting(index_name, year, month).value
+    return end_price * taxes
+
+
+def handler(event, _context):
+    """The handler"""
+    last_month = date.today().replace(day=1) - timedelta(days=1)
+    question_type = event["TYPE"]
+
+    if question_type == "index":
+        req_index = event["INDEX"]
+        req_year = event.get("YEAR", last_month.year)
+        req_month = event.get("MONTH", last_month.month)
+
+        indexing_setting = get_indexing_setting(req_index, req_year, req_month)
+        return {"statusCode": 200, "body": json.dumps(asdict(indexing_setting), default=str)}
+    elif question_type == "end_price":
+        req_index = event["INDEX"]
+        req_year = event.get("YEAR", last_month.year)
+        req_month = event.get("MONTH", last_month.month)
+        intercept = event["INTERCEPT"]
+        slope = event["SLOPE"]
+        taxes = event["TAXES"]
+        end_price = get_end_price(req_index, req_year, req_month, slope, intercept, taxes)
+        return {"statusCode": 200, "body": json.dumps({"end_price": end_price}, default=str)}
+    return {"statusCode": 400}

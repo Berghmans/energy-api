6382c2ea2795e8005e876e11023c8ed4e5d49255
Olivier Berghmans <olivier@cloudar.be>
2023-05-12 18:17:13 +0200

    Add logging for feeder

--- app/energy_tariffs/lambda_engie.py
+++ app/energy_tariffs/lambda_engie.py
@@ -1,9 +1,14 @@
 """Lambda handler for the Engie feeder"""
 import os
+import logging
 
 import boto3
 
 from engie import EngieIndexingSetting, GAS_URL, ENERGY_URL
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def get_index_values() -> list[EngieIndexingSetting]:
@@ -16,4 +21,5 @@ def handler(_event, _context):
     index_values = get_index_values()
     dynamodb = boto3.resource("dynamodb")
     db_table = dynamodb.Table(os.environ["TABLE_NAME"])
+    logger.info(f"Sending {len(index_values)} indexing settings to the database")
     EngieIndexingSetting.save_list(db_table, index_values)

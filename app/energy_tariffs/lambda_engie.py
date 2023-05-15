496e9b4c29998e09030ebe88db7afd5e6d8abbca
Olivier Berghmans <olivier@cloudar.be>
2023-05-15 11:27:07 +0200

    Add EPEX DAM calculation

--- app/energy_tariffs/lambda_engie.py
+++ app/energy_tariffs/lambda_engie.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def handler(_event, _context):
+def handler(event, _context):
     """The handler"""
     not_before = datetime.now() - timedelta(days=90)
     index_values = EngieIndexingSetting.get_gas_values(not_before) + EngieIndexingSetting.get_energy_values(not_before)
@@ -21,3 +21,11 @@ def handler(_event, _context):
     db_table = dynamodb.Table(os.environ["TABLE_NAME"])
     logger.info(f"Sending {len(index_values)} indexing settings to the database")
     EngieIndexingSetting.save_list(db_table, index_values)
+
+    # Derived values
+    calculation_date = None
+    if "calculate" in event:
+        calculation_date = datetime.strptime(event["calculate"], "%Y/%m/%d").date()
+    derived_values = EngieIndexingSetting.calculate_derived_values(db_table, calculation_date)
+    logger.info(f"Sending {len(derived_values)} derived indexing settings to the database")
+    EngieIndexingSetting.save_list(db_table, derived_values)
